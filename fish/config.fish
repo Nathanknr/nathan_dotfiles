5c60d2d11a4e66ad30410a2cd0371e19aebde40f
Nathanknr <nathankamgang4270@gmail.com>
2026-05-10 08:13:09 +0400

    feat: start tracking fish configuration

---
+++ fish/config.fish
@@ -0,0 +1,20 @@
+zoxide init fish | source 
+function tt
+	taskwarrior-tui --report list
+end 
+
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+# Lazy conda — only loads when you actually type `conda`
+function conda
+    functions --erase conda
+    if test -f "$HOME/anaconda3/etc/fish/conf.d/conda.fish"
+        source "$HOME/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        eval "$HOME/anaconda3/bin/conda" shell.fish hook $argv | source
+    end
+    conda $argv
+end
+
+
+set -g fish_key_bindings fish_vi_key_bindings
