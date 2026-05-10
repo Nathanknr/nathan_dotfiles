db763c026b2bc5737f1149acf0389fea2e6e1cd1
Nathanknr <nathankamgang4270@gmail.com>
2026-05-10 11:03:44 +0400

    feat: add telescope plugin and its keybindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,6 +30,14 @@ require("lazy").setup({
       end,
     },
     {
+    'nvim-telescope/telescope.nvim', version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    }
+},
+    {
   "pxwg/math-conceal.nvim",
   event = "VeryLazy",
   main = "math-conceal",
@@ -130,6 +138,11 @@ require("lazy").setup({
 vim.opt.number = true
 vim.opt.conceallevel = 2
 vim.opt.concealcursor = ""
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set("n", "<leader>u", function()
   vim.cmd("call UltiSnips#RefreshSnippets()")
 end, { desc = "Refresh UltiSnips" })
