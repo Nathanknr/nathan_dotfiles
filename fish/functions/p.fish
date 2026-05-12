db0fcbfdb87fbf30321bf7be49a721318b4deb9f
Nathanknr <nathankamgang4270@gmail.com>
2026-05-12 08:49:02 +0400

    feat: create a function for searching pdf using fast-p and fzf

---
+++ fish/functions/p.fish
@@ -0,0 +1,13 @@
+# ~/.config/fish/functions/p.fish
+
+function p
+    set open xdg-open
+    ag -U -g '.pdf$' \
+    | fast-p \
+    | fzf --read0 --reverse -e -d (printf '\t') \
+        --preview-window down:80% --preview '
+            v=$(echo {q} | tr " " "|");
+            echo -e {1}"\n"{2} | grep -E "^|$v" -i --color=always;
+        ' \
+    | cut -z -f 1 -d (printf '\t') | tr -d '\n' | xargs -r --null $open > /dev/null 2> /dev/null
+end
