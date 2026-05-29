d584cab517d4c7f693d04da4157128166aef7cb1
Nathanknr <nathankamgang4270@gmail.com>
2026-05-29 20:00:13 +0400

    feat: switch to multifile config

---
+++ nvim/lua/plugins/OrganizeThis.lua
@@ -0,0 +1,132 @@
+ return {
+	 {
+  "lervag/vimtex",
+  lazy = false,
+  init = function()
+    vim.g.vimtex_view_method = "zathura"
+    vim.g.tex_conceal = "abdmg"
+
+    local save_timer = nil
+
+    vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+      pattern = "*.tex",
+      callback = function()
+        if save_timer then
+          save_timer:stop()
+          save_timer:close()
+        end
+        save_timer = vim.uv.new_timer()
+        save_timer:start(500, 0, vim.schedule_wrap(function()
+          vim.cmd("silent! write")
+          save_timer:close()
+          save_timer = nil
+        end))
+      end,
+    })
+  end,
+},
+{
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+},
+ {
+  "pxwg/math-conceal.nvim",
+  event = "VeryLazy",
+  main = "math-conceal",
+  --- @type LaTeXConcealOptions
+  opts = {
+    conceal = {
+      "greek",
+      "script",
+      "math",
+      "font",
+      "delim",
+      "phy",
+    },
+    ft = { "plaintex", "tex", "context", "bibtex", "markdown", "typst" },
+  },
+},
+    {
+      "m4xshen/hardtime.nvim",
+      lazy = false,
+      dependencies = { "MunifTanjim/nui.nvim" },
+      opts = {},
+    },
+    {
+
+  'chomosuke/typst-preview.nvim',
+
+  lazy = false, -- or ft = 'typst'
+
+  version = '1.*',
+
+  opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+
+},
+{
+  "vimwiki/vimwiki",
+  lazy = false,
+  priority = 1000,  -- Load first
+  init = function()
+    vim.g.vimwiki_list = {
+      {
+        path = "~/vimwiki/",
+        syntax = "default",
+        ext = ".wiki",
+      },
+    }
+  end,
+},
+{
+
+  "mason-org/mason.nvim",
+
+  opts = {
+
+    ensure_installed = {
+
+      "tinymist",
+      "pyright"
+
+    },
+
+  },
+
+},
+   {
+      'stevearc/oil.nvim',
+      opts = {},
+      dependencies = { { "echasnovski/mini.icons", opts = {} } },
+      lazy = false,
+    },
+    
+    {
+  {
+      "SirVer/ultisnips",
+      event = "InsertEnter",
+      lazy = false, 
+      init = function()
+        vim.g.UltiSnipsSnippetDirectories = { "/home/nathan/.config/Ultisnips" }
+        vim.g.UltiSnipsExpandTrigger = "<tab>"
+        vim.g.UltiSnipsJumpForwardTrigger = "<tab>"
+        vim.g.UltiSnipsJumpBackwardTrigger = "<C-k>"
+        vim.g.UltiSnipsEditSplit = "vertical"
+      end,
+    },
+  },
+}
+  
