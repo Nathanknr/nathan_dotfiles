4a0fbd473bcde0b2d5705a42568d1d53172b62f3
Nathanknr <nathankamgang4270@gmail.com>
2026-05-30 13:05:21 +0400

    feat: use vimtex for conceal and not treesitter

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	lazy = false,
+	config = function()
+		require("nvim-treesitter").setup({
+			ensure_installed = {
+				"bash",
+				"c",
+				"html",
+				"lua",
+				"markdown",
+				"vim",
+				"vimdoc",
+			},
+			ignore_install = { "latex" }, -- never auto-install latex parser
+
+			auto_install = true,
+			highlight = { enable = true, disable = { "latex" } },
+			indent = { enable = true, disable = { "latex" } },
+		})
+	end,
+}
