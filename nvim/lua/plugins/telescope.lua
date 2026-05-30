7729be4e09faa9bf81857d1b741258f69ca22438
Nathanknr <nathankamgang4270@gmail.com>
2026-05-30 18:48:13 +0400

    feat: add keybind to grep .config in nvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,47 +1,46 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  version = "*",
+	"nvim-telescope/telescope.nvim",
+	version = "*",
 
-  dependencies = {
-    "nvim-lua/plenary.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
 
-    -- Faster fuzzy matching (optional but recommended)
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-    },
-  },
+		-- Faster fuzzy matching (optional but recommended)
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
+	},
 
-  config = function()
-    local telescope = require("telescope")
-    local builtin = require("telescope.builtin")
+	config = function()
+		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
 
-    telescope.setup({
-      defaults = {
-        -- you can tweak layout here
-        layout_config = {
-          horizontal = { preview_width = 0.55 },
-        },
-      },
-    })
+		telescope.setup({
+			defaults = {
+				-- you can tweak layout here
+				layout_config = {
+					horizontal = { preview_width = 0.55 },
+				},
+			},
+		})
 
-    -- Keymaps (plugin-specific)
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
-    vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Commands" })
-vim.keymap.set("n", "<leader>uu", function()
-  builtin.find_files({ cwd = "~/.config/Ultisnips/" })
-end, { desc = "Search UltiSnips" })
+		-- Keymaps (plugin-specific)
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+		vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Commands" })
+		vim.keymap.set("n", "<leader>uu", function()
+			builtin.find_files({ cwd = "~/.config/Ultisnips/" })
+		end, { desc = "Search UltiSnips" })
 
-vim.keymap.set("n", "<leader>nn", function()
-  builtin.find_files({ cwd = "~/.config/nvim/" })
-end, { desc = "Search neovim config" })
+		vim.keymap.set("n", "<leader>nn", function()
+			builtin.find_files({ cwd = "~/.config/nvim/" })
+		end, { desc = "Search neovim config" })
 
-end
-
+		vim.keymap.set("n", "<leader>jj", function()
+			builtin.find_files({ cwd = "~/.config//" })
+		end, { desc = "Search neovim config" })
+	end,
 }
-
-
-
