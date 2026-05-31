a8c1826567859bda09ac17a9e3767e72545b80f2
Nathanknr <nathankamgang4270@gmail.com>
2026-05-31 11:43:14 +0400

    feat: add function in fish to send video and audio to ipad

---
+++ fish/functions/sendvid.fish
@@ -0,0 +1,8 @@
+# Send audio and video to my Ipad 
+set FILE (find . -maxdepth 2 \( -name '*.mp3' -o -name '*.flac' -o -name '*.wav' -o -name '*.ogg' -o -name '*.m4a' -o -name '*.aac' -o -name '*.opus' -o -name '*.mp4' -o -name '*.mkv' -o -name '*.avi' -o -name '*.webm' -o -name '*.mov' \) | fzf)
+
+if test -z "$FILE"
+    exit 1
+end
+
+kdeconnect-cli --device 9162DD724B104D6397E00D29F6E64155 --share "$FILE"
