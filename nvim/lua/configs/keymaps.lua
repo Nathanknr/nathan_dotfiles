160e28510fa7c5793323dc4c97fe7d46534f7f07
Nathanknr <nathankamgang4270@gmail.com>
2026-05-31 10:17:07 +0400

    feat: test prime's remaps

--- nvim/lua/configs/keymaps.lua
+++ nvim/lua/configs/keymaps.lua
@@ -1,7 +1,31 @@
 local map = vim.keymap.set
-map('n','<leader>jj', '<cmd>Oil<cr>')
-map('i','jk', '<Esc>')
-map('i','kj', '<Esc>')
+map("n", "<leader>jj", "<cmd>Oil<cr>")
+map("i", "jk", "<Esc>")
+map("i", "kj", "<Esc>")
 
+vim.g.mapleader = " "
 
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "=ap", "ma=ap'a")
 
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
+
+vim.keymap.set("n", "<leader>l", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader><leader>", function()
+	vim.cmd("so")
+end)
+
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
