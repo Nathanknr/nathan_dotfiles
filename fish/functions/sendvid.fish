a319e6407f64c891e4eaefb10faadf249418a2db
Nathanknr <nathankamgang4270@gmail.com>
2026-05-31 11:55:30 +0400

    fix: repair the sendvid script

--- fish/functions/sendvid.fish
+++ fish/functions/sendvid.fish
@@ -1,8 +1,12 @@
+
 # Send audio and video to my Ipad 
-set FILE (find . -maxdepth 2 \( -name '*.mp3' -o -name '*.flac' -o -name '*.wav' -o -name '*.ogg' -o -name '*.m4a' -o -name '*.aac' -o -name '*.opus' -o -name '*.mp4' -o -name '*.mkv' -o -name '*.avi' -o -name '*.webm' -o -name '*.mov' \) | fzf)
+function sendvid
+	set FILE (find . -maxdepth 2 \( -name '*.mp3' -o -name '*.flac' -o -name '*.wav' -o -name '*.ogg' -o -name '*.m4a' -o -name '*.aac' -o -name '*.opus' -o -name '*.mp4' -o -name '*.mkv' -o -name '*.avi' -o -name '*.webm' -o -name '*.mov' \) | fzf)
 
-if test -z "$FILE"
-    exit 1
-end
+	if test -z "$FILE"
+		return 1
+	end
 
-kdeconnect-cli --device 9162DD724B104D6397E00D29F6E64155 --share "$FILE"
+	kdeconnect-cli --device 9162DD724B104D6397E00D29F6E64155 --share "$FILE"
+
+end 
