cc813ad72e64e6447060c0c55a5d9a13a3d400f1
Nathanknr <nathankamgang4270@gmail.com>
2026-06-07 23:39:52 +0400

    feat: add the logic functions for meds script

--- scripts/atomoxetine.py
+++ scripts/atomoxetine.py
@@ -1,12 +1,15 @@
 # This script will do some things
 # This scripts knows how much atomoxetine I have remaining
-
 # This script reminds me on whatsapp to drink my meds
-
 # This script update the atomoxetine count each time it send a message
 # This script tells me the number of pills in the solely opened box assuming that I drank it
-
 from datetime import timedelta, datetime
+import os
+import pywhatkit
+from dotenv import load_dotenv
+
+load_dotenv()
+NATHAN_NUM = os.environ.get("NATHAN_NUM")
 
 class PillStock:
     def __init__(self, dosePerPill, prescribedDose, stock, splitDose):
@@ -16,6 +19,27 @@ class PillStock:
         self.numberOfPillsPerDay = prescribedDose / dosePerPill
         self.numberOfDaysCovered = stock / self.numberOfPillsPerDay
         self.dateWhenStockEnds = datetime.now() + timedelta(days=self.numberOfDaysCovered)
+        self.numberOfDosesTakenToday = 0
         self.splitDose = splitDose
 
+    def updateStock(self):
+        if self.splitDose:
+            self.stock = self.stock - self.numberOfPillsPerDay / 2
+        else:
+            self.stock = self.stock - self.numberOfPillsPerDay
 
+    def recompute(self):
+        self.numberOfDaysCovered = self.stock / self.numberOfPillsPerDay
+        self.dateWhenStockEnds = datetime.now() + timedelta(days=self.numberOfDaysCovered)
+
+    def drinkMeds(self):
+        if self.splitDose and self.numberOfDosesTakenToday in (0, 1):
+            pywhatkit.sendwhatmsg_instantly(NATHAN_NUM, f'Nathan drink {self.numberOfPillsPerDay / 2} pills', 10)
+            self.numberOfDosesTakenToday += 1
+            self.updateStock()
+            self.recompute()
+        elif not self.splitDose and self.numberOfDosesTakenToday == 0:
+            pywhatkit.sendwhatmsg_instantly(NATHAN_NUM, f'Nathan drink {self.numberOfPillsPerDay} pills', 10)
+            self.numberOfDosesTakenToday += 1
+            self.updateStock()
+            self.recompute()
