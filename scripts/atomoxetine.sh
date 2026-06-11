af643b353ce0cf335d3c61eb40807da3abf7d298
Nathanknr <nathankamgang4270@gmail.com>
2026-06-11 17:15:49 +0400

    fix: improve your method to solve display problem

--- scripts/atomoxetine.sh
+++ scripts/atomoxetine.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-
+# Systemd runs headless with no display. Exporting DISPLAY and XAUTHORITY
+# passes them down to all child processes including Python and the browser.
+#brave-browser &
+#export DISPLAY=:0
+#export XAUTHORITY=/home/nathan/.Xauthority
+#brave-browser &
+#sleep 3
 cd /home/nathan/.config/scripts
 source .venv/bin/activate
 python3 atomoxetine.py
--- scripts/remind_landlord.sh
+++ scripts/remind_landlord.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+#export DISPLAY=:0
+#export XAUTHORITY=/home/nathan/.Xauthority
+# Systemd runs headless with no display. Exporting DISPLAY and XAUTHORITY
+# passes them down to all child processes including Python and the browser.
+#brave-browser &
+#sleep 3
 cd /home/nathan/.config/scripts
 source .venv/bin/activate
 python3 remind_landlord.py
-
-
-	
