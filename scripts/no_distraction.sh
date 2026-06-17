aa9a3d3d5c93619717dacd6181c4c47e47b4d281
Nathanknr <nathankamgang4270@gmail.com>
2026-06-17 13:24:14 +0400

    fix: fix the commands in no_distractions.sh

--- scripts/no_distraction.sh
+++ scripts/no_distraction.sh
@@ -7,57 +7,58 @@ pluck + when 0-9 block port:443
 pluck + import:9623d022-89f3-4914-a436-5a834261f172
 
 
-action:allow	host:github.com
-action:allow	port:443
-action:allow	program:.local/share/applications/
-action:allow	program:/home/nathan/.cargo/bin/cargo
-action:allow	program:/usr/bin
-action:allow	program:/usr/bin/git
-action:allow	program:/usr/bin/lutris	host:google.com	mediaType:application/
-action:allow	program:/usr/bin/python3.13
-action:allow	program:/usr/bin/steam
-action:allow	program:cargo
-action:allow	program:python3.13
-action:allow	program:todoist
-action:block	everything:true	when:0-7,2130-24
-action:block	host:bsky.app
-action:block	host:facebook.com
-action:block	host:flickystream.ru
-action:block	host:instagram.com
-action:block	host:reddit.com
-action:block	host:tiktok.com
-action:block	host:x.com
-action:block	host:youtube.com
-action:block	program:/ net.ankiweb.Anki/ org.sqlitebrowser.sqlitebrowser/ re.sonny.Eloquent/
-action:block	program:/home/nathan/.local/bin/zen
-action:block	program:/home/nathan/.local/share/flatpak/
-action:block	program:/home/nathan/.local/share/flatpak/app/org.torproject.torbrowser-launcher
-action:block	program:/home/nathan/.local/share/flatpak/exports/bin
-action:block	program:/home/nathan/.nix-profile/bin
-action:block	program:/home/nathan/.nix-profile/bin/tor-browser
-action:block	program:/home/nathan/.var/app/org.torproject.torbrowser-launcher
-action:block	program:/home/nathan/local/share/flatpak
-action:block	program:/nix/store/
-action:block	program:/nix/var/nix/profiles/default/bin/nix
-action:block	program:/nix/var/nix/profiles/default/bin/nix-env
-action:block	program:/usr/bin/chromium
-action:block	program:/usr/bin/python3.13
-action:block	program:/usr/bin/qutebrowser
-action:block	program:/var/lib/flatpak/app/org.torproject.torbrowser-launcher/
-action:block	program:chrome
-action:block	program:chromium
-action:block	program:firefox
-action:block	program:nix
-action:block	program:qbittorrent
-action:block	program:qutebrowser
-action:block	program:tor
-action:block	program:tor-browser
-action:block	program:~/.local/share/flatpak/app/org.torproject.torbrowser-launcher/
-feature:nofirefox
-feature:okedge
-feature:safe
-feature:safeplease
-feature:system
+pluck + allow	host:github.com
+pluck + allow	port:443
+pluck + allow	program:.local/share/applications/
+pluck + allow	program:/home/nathan/.cargo/bin/cargo
+pluck + allow	program:/usr/bin
+pluck + allow	program:/usr/bin/git
+pluck + allow	program:/usr/bin/lutris	host:google.com	mediaType:application/
+pluck + allow	program:/usr/bin/python3.13
+pluck + allow	program:/usr/bin/steam
+pluck + allow	program:cargo
+pluck + allow	program:python3.13
+pluck + allow	program:todoist
+pluck + block	everything:true	when:0-7,2130-24
+pluck + block	host:bsky.app
+pluck + block	host:facebook.com
+pluck + block	host:flickystream.ru
+pluck + block	host:instagram.com
+pluck + block	host:reddit.com
+pluck + block	host:tiktok.com
+pluck + block	host:x.com
+pluck + block	host:youtube.com
+pluck + block	program:/ net.ankiweb.Anki/ org.sqlitebrowser.sqlitebrowser/ re.sonny.Eloquent/
+pluck + block	program:/home/nathan/.local/bin/zen
+pluck + block	program:/home/nathan/.local/share/flatpak/
+pluck + block	program:/home/nathan/.local/share/flatpak/app/org.torproject.torbrowser-launcher
+pluck + block	program:/home/nathan/.local/share/flatpak/exports/bin
+pluck + block	program:/home/nathan/.nix-profile/bin
+pluck + block	program:/home/nathan/.nix-profile/bin/tor-browser
+pluck + block	program:/home/nathan/.var/app/org.torproject.torbrowser-launcher
+pluck + block	program:/home/nathan/local/share/flatpak
+pluck + block	program:/nix/store/
+pluck + block	program:/nix/var/nix/profiles/default/bin/nix
+pluck + block	program:/nix/var/nix/profiles/default/bin/nix-env
+pluck + block	program:/usr/bin/chromium
+pluck + block	program:/usr/bin/python3.13
+pluck + block	program:/usr/bin/qutebrowser
+pluck + block	program:/var/lib/flatpak/app/org.torproject.torbrowser-launcher/
+pluck + block	program:chrome
+pluck + block	program:chromium
+pluck + block	program:firefox
+pluck + block	program:nix
+pluck + block	program:qbittorrent
+pluck + block	program:qutebrowser
+pluck + block	program:tor
+pluck + block	program:tor-browser
+pluck + block	program:~/.local/share/flatpak/app/org.torproject.torbrowser-launcher/
+pluck + nofirefox
+pluck + okedge
+pluck + safe
+pluck + safeplease
+pluck + system
+pluck + system                # enable the system feature (aka level 2)
 
 cd /run/media/nathan/Ventoy
 rm *.iso
