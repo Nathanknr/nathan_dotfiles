66434c0594b53527488db8b6f048fb114b94bb28
Nathanknr <nathankamgang4270@gmail.com>
2026-06-17 12:09:54 +0400

    feat: go to sleep earlier

--- scripts/sleep_early.sh
+++ scripts/sleep_early.sh
@@ -2,7 +2,7 @@
 # I will power off my pc every five minutes to remind myself that I shouldn't have the pc turned on at this after 11pm
 # hope it stops me from pushing commit to your dotfiles at 1am
 #
-notify-send "Going down baby " "Shutting off in 10 seconds. You should be asleep nigger" -u critical
+notify-send "Going down baby " "Shutting off in 10 seconds. No device in your bedtime window" -u critical
 sleep 10
 
 systemctl poweroff
