17cc4a14acd206cba6c7719eacfed48a4e7b6389
Nathanknr <nathankamgang4270@gmail.com>
2026-07-12 12:19:39 +0400

    fix: remove duplicate in functions from problem.py

--- scripts/problem.py
+++ scripts/problem.py
@@ -17,18 +17,6 @@ def addProblemToday(difficulty, hint):
     addProblem(difficulty, hint, today)
 
 
-# This function will take a text file and parse each will with int,int and add it to db
-def bulkAddProblemToday(text_file):
-    with open(text_file,'r') as file:
-        text = file.read()
-        object = re.compile(r'([0-9]),([0-9])\n')
-        match = object.findall(text)
-        for i in range(0,len(match)):
-            difficulty = match[i][0]
-            hint = match[i][1]
-            addProblemToday(difficulty,hint)
-        con.commit()
-
 
 def bulkAddProblem(text_file, day):
         with open(text_file,'r') as file:
@@ -40,3 +28,6 @@ def bulkAddProblem(text_file, day):
                 hint = match[i][1]
                 addProblem(difficulty,hint,day)
             con.commit()
+
+def bulkAddProblemToday(text_file):
+    bulkAddProblem(text_file, today)
