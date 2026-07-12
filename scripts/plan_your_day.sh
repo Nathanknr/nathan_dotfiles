5d55920a7d8f6eebad63e4589da7ca946440c9cc
Nathanknr <nathankamgang4270@gmail.com>
2026-07-12 12:19:00 +0400

    feat: start timewarrior when planning your day

--- scripts/plan_your_day.sh
+++ scripts/plan_your_day.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 # md is probably better and more lightweight but oh boy do i love a  pretty pdf
+timew stop
+task add "plan your day" | grep -oE '[0-9]+' | xargs task start 
 cd /home/nathan/Resources/Notes/pdf
 rm plan.tex
 touch plan.tex
