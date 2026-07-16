7db123d2c2c724d0b3243d78a75fdf026396db11
Nathanknr <nathankamgang4270@gmail.com>
2026-07-16 16:00:15 +0400

    fix: remove uncessary config in vimtex

--- nvim/lua/plugins/OrganizeThis.lua
+++ nvim/lua/plugins/OrganizeThis.lua
@@ -3,7 +3,6 @@ return {
 		"lervag/vimtex",
 		lazy = false,
 		init = function()
-			vim.g.vimtex_view_method = "zathura"
 			vim.g.vimtex_syntax_enabled = 1
 			vim.g.vimtex_syntax_conceal_disable = 0
 
