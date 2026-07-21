5fa4b94c9402627e254c1de866a0f3ce4a4f65d2
Nathanknr <nathankamgang4270@gmail.com>
2026-07-21 19:43:33 +0400

    feat: configure conform nvim

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,14 +1,42 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {
-		notify_on_error = false,
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "ruff" },
-		},
-	},
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      notify_on_error = false,
+
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+
+      formatters_by_ft = {
+        lua = { "stylua" },
+
+        python = {
+          "ruff_organize_imports",
+          "ruff_format",
+        },
+
+        javascript = {
+          "prettierd",
+          "prettier",
+        },
+
+        html = {
+          "prettierd",
+          "prettier",
+        },
+
+        css = {
+          "prettierd",
+          "prettier",
+        },
+      },
+    },
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+    opts = {},
+  },
 }
