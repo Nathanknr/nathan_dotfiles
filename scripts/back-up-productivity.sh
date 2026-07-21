133ea982dfff4386b515ea79f909e235a571d10e
Nathanknr <nathankamgang4270@gmail.com>
2026-07-21 19:56:26 +0400

    feat: back up my notes

--- scripts/back-up-productivity.sh
+++ scripts/back-up-productivity.sh
@@ -2,3 +2,4 @@
 
 rsync -a /home/nathan/.config/task/.task /home/nathan/Dropbox/backups
 rsync -a /home/nathan/.local/share/timewarrior /home/nathan/Dropbox/backups
+rsync -a /home/nathan/Resources/Notes/pdf/pdf/plan.pdf /home/nathan/Dropbox/backups
