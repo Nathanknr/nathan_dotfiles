88e4b74ffec1146a5ce0465d2f6cefbb1db15ea7
Nathanknr <nathankamgang4270@gmail.com>
2026-07-21 19:42:40 +0400

    feat: configure LSPAttach

--- nvim/lua/configs/keymaps.lua
+++ nvim/lua/configs/keymaps.lua
@@ -29,3 +29,35 @@ end)
 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- keymaps
+vim.api.nvim_create_autocmd(
+	"LspAttach",
+	{ --  Use LspAttach auto command to only map the following keys after the language server attaches to the current buffer
+		group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+		callback = function(ev)
+			vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc" -- Enable completion triggered by <c-x><c-o>
+
+			local opts = function(desc)
+				return { buffer = ev.buf, silent = true, desc = desc }
+			end
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
+			vim.keymap.set("n", "<leader><space>", vim.lsp.buf.hover, opts("Hover documentation"))
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Go to implementation"))
+			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts("Go to type definition"))
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts("Rename symbol"))
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts("Find references"))
+
+			-- vim.keymap.set({ "n", "v" }, "<leader>ca", function()
+			-- 	require("tiny-code-action").code_action()
+			-- end, opts("Code action"))
+			vim.keymap.set("n", "<leader>o", vim.lsp.buf.format, opts("Format buffer"))
+
+			vim.keymap.set("n", "<leader>d", function()
+				vim.diagnostic.open_float({
+					border = "rounded",
+				})
+			end, opts("Show diagnostics float"))
+		end,
+	}
+)
--- nvim/lua/plugins/LSP.lua
+++ nvim/lua/plugins/LSP.lua
@@ -1,23 +1,25 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {},
-  },
-  {
-    "neovim/nvim-lspconfig",
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "rust_analyzer",
-	"pyright",
-	"lua_ls" 
-      },
-      automatic_enable = true,
-    },
-  },
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.diagnostic.config({
+				virtual_text = true,
+			})
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"rust_analyzer",
+				"pyright",
+				"lua_ls",
+			},
+			automatic_enable = true,
+		},
+	},
 }
-  
-  
-
