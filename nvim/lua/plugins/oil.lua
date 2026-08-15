285fe3ebe93688caba85938f8a5a6e35b6d0fd50
Nathanknr <nathankamgang4270@gmail.com>
2026-08-15 17:41:09 +0400

    feat: improve lua formatting in nvim config

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,8 +1,6 @@
-return  {
-      'stevearc/oil.nvim',
-      opts = {},
-      dependencies = { { "echasnovski/mini.icons", opts = {} } },
-      lazy = false,
-      }
-    
- 
+return {
+	"stevearc/oil.nvim",
+	opts = {},
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	lazy = false,
+}
